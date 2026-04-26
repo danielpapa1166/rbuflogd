6fc9f02deebc275f1599c58fca8a8ed80153832f
Dániel Pápa <danielpapa1166@gmail.com>
2026-04-26 13:37:28 +0200

    minor refactor in log sink

--- src/daemon/log_sink.c
+++ src/daemon/log_sink.c
@@ -4,8 +4,9 @@
 #include <sys/stat.h>
 #include <string.h>
 
-#define LOG_FILE_MAX_BYTES (1024U * 1024U)
-#define LOG_FILE_INDEX_MAX 1000U
+#define LOG_FILE_MAX_BYTES        (1024U * 1024U)
+#define LOG_FILE_INDEX_MAX        1000U
+#define LOG_FILE_NAME_FORMAT      "rbuflogd_%03u.log"
 
 static FILE *log_file = NULL;
 static unsigned current_log_index = 0;
@@ -16,8 +17,13 @@ static int build_log_file_path(char * out_path, size_t out_path_sz, unsigned ind
   if (out_path == NULL || out_path_sz == 0) {
     return -1;
   }
+  const int res = snprintf(
+    out_path, 
+    out_path_sz, 
+    LOG_FILE_NAME_FORMAT, 
+    index); 
 
-  if (snprintf(out_path, out_path_sz, "rbuflogd_%03u.log", index) < 0) {
+  if (res < 0) {
     return -1;
   }
 
@@ -37,12 +43,15 @@ static int find_log_start(unsigned * out_index, size_t * out_size, const char **
   for (unsigned i = 0; i < LOG_FILE_INDEX_MAX; i++) {
     char path[32];
     struct stat st;
+    const int path_build_res = build_log_file_path(
+      path, sizeof(path), i); 
 
-    if (build_log_file_path(path, sizeof(path), i) != 0) {
+    if (path_build_res != 0) {
       return -1;
     }
 
-    if (stat(path, &st) != 0) {
+    const int stat_res = stat(path, &st);
+    if (stat_res != 0) {
       continue;
     }
 
@@ -65,8 +74,6 @@ static int find_log_start(unsigned * out_index, size_t * out_size, const char **
     *out_size = (size_t) highest_stat.st_size;
     *out_mode = "a";
 
-    printf("Resuming log file rbuflogd_%03u.log at size %zu bytes\n", highest_index, (size_t) highest_stat.st_size);
-
     return 0;
   }
 
@@ -79,7 +86,9 @@ static int find_log_start(unsigned * out_index, size_t * out_size, const char **
 static int open_log_file(unsigned index, const char * mode, size_t initial_size) {
   char path[32];
 
-  if (build_log_file_path(path, sizeof(path), index) != 0) {
+  const int res = build_log_file_path(
+    path, sizeof(path), index);
+  if (res != 0) {
     return -1;
   }
 
@@ -112,11 +121,15 @@ int rbuflogd_log_sink_init(void) {
   size_t size;
   const char * mode;
 
-  if (find_log_start(&index, &size, &mode) != 0) {
+  int res; 
+
+  res = find_log_start(&index, &size, &mode); 
+  if (res != 0) {
     return -1;
   }
 
-  if (open_log_file(index, mode, size) != 0) {
+  res = open_log_file(index, mode, size); 
+  if (res != 0) {
     return -1;
   }
 
@@ -126,32 +139,45 @@ int rbuflogd_log_sink_init(void) {
 
 int rbuflogd_write_log(const char * log_msg) {
   size_t line_bytes;
+  int res; 
 
   if (log_msg == NULL) {
     return -1;
   }
 
-  if (!log_sink_initialized && rbuflogd_log_sink_init() != 0) {
-    return -1;
+  if (!log_sink_initialized) {
+    res = rbuflogd_log_sink_init();
+    if(res != 0) {
+      return -1;
+    }
   }
 
-  if (log_file == NULL && open_log_file(current_log_index, "a", current_log_size) != 0) {
-    return -1;
+  if (log_file == NULL) {
+    res = open_log_file(
+      current_log_index,
+      "a", 
+      current_log_size);
+    if(res != 0) {
+      return -1;
+    }
   }
 
   line_bytes = strlen(log_msg) + 1U; // message + newline
 
   if (current_log_size > 0 && (current_log_size + line_bytes) > LOG_FILE_MAX_BYTES) {
-    if (rotate_log_file() != 0) {
+    res = rotate_log_file();
+    if(res != 0) {
       return -1;
     }
   }
 
-  if (fprintf(log_file, "%s\n", log_msg) < 0) {
+  res = fprintf(log_file, "%s\n", log_msg);
+  if (res < 0) {
     return -1;
   }
 
-  if (fflush(log_file) != 0) {
+  res = fflush(log_file);
+  if (res != 0) {
     return -1;
   }
 
