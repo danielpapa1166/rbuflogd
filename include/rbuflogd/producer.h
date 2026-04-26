9793daba78f2e22200eecc3ce8527b780c26178e
Dániel Pápa <danielpapa1166@gmail.com>
2026-04-26 19:53:12 +0200

    extern C block added to use producer lib in c++ application

--- include/rbuflogd/producer.h
+++ include/rbuflogd/producer.h
@@ -3,6 +3,10 @@
 
 #include "rbuflogd/pub_common_types.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 
 typedef struct {
   char   producer_name[RBUF_PROD_ID_MAX_LEN];
@@ -13,5 +17,9 @@ int rbuflogd_producer_open(rbuflogd_producer_t * producer, const char * producer
 int rbuflogd_producer_log(rbuflogd_producer_t * producer, 
   rbuflogd_log_level_t level, const char * category, const char * log_msg);
 void rbuflogd_producer_close(rbuflogd_producer_t * producer);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* RBUFLOGD_PRODUCER_H */
