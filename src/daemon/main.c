8fb212b9765cf7c7ef79e960e68ecfbffa904035
Dániel Pápa <danielpapa1166@gmail.com>
2026-04-26 13:49:20 +0200

    restructure in main file

--- src/daemon/main.c
+++ src/daemon/main.c
@@ -8,46 +8,16 @@
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
-
-#define BOOT_ID_PATH "/proc/sys/kernel/random/boot_id"
-
-static int read_boot_id(char * out_boot_id, size_t out_boot_id_sz) {
-  FILE * f = NULL;
-  size_t len;
-
-  if (out_boot_id == NULL || out_boot_id_sz == 0) {
-    return -1;
-  }
-
-  f = fopen(BOOT_ID_PATH, "r");
-  if (f == NULL) {
-    return -1;
-  }
-
-  if (fgets(out_boot_id, (int) out_boot_id_sz, f) == NULL) {
-    fclose(f);
-    return -1;
-  }
-
-  fclose(f);
-  len = strlen(out_boot_id);
-  if (len > 0 && out_boot_id[len - 1] == '\n') {
-    out_boot_id[len - 1] = '\0';
-  }
-
-  return 0;
-}
-
 // note: compile with: 
 // rm -rf build && cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON && cmake --build build
+
+#define BOOT_ID_PATH "/proc/sys/kernel/random/boot_id"
+static int read_boot_id(char * out_boot_id, size_t out_boot_id_sz); 
+void handle_signal(int signal); 
 
 // handle user terminate signal to cleanup shared memory
 static int terminate_app = 0;
 
-void handle_signal(int signal) {
-  (void) signal; // unused parameter
-  terminate_app = 1;
-}
 
 int main(void) {
   char boot_id[RBUF_BOOT_ID_MAX_CHARS + 1] = {0};
@@ -92,4 +62,38 @@ int main(void) {
   printf("Consumer has terminated.\n");
 
   return 0;
+}
+
+
+
+static int read_boot_id(char * out_boot_id, size_t out_boot_id_sz) {
+  FILE * f = NULL;
+  size_t len;
+
+  if (out_boot_id == NULL || out_boot_id_sz == 0) {
+    return -1;
+  }
+
+  f = fopen(BOOT_ID_PATH, "r");
+  if (f == NULL) {
+    return -1;
+  }
+
+  if (fgets(out_boot_id, (int) out_boot_id_sz, f) == NULL) {
+    fclose(f);
+    return -1;
+  }
+
+  fclose(f);
+  len = strlen(out_boot_id);
+  if (len > 0 && out_boot_id[len - 1] == '\n') {
+    out_boot_id[len - 1] = '\0';
+  }
+
+  return 0;
+}
+
+void handle_signal(int signal) {
+  (void) signal; // unused parameter
+  terminate_app = 1;
 }
